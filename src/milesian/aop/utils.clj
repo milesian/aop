459f3fece3d7a4bafa1b652ba274a21868cc442f
tangrammer <juanantonioruz@gmail.com>
2014-12-05 13:16:34 +0100

    Add logging utils

--- src/milesian/aop/utils.clj
+++ src/milesian/aop/utils.clj
@@ -1,7 +1,7 @@
 (ns milesian.aop.utils
   (:require [clojure.string :as st]))
 
-(defn function-invocation
+(defn extract-data
   [*fn* this args]
   (let [component-key (:bigbang/key (meta this))
         [fn-name fn-args]((juxt :function-name :function-args) (meta *fn*))
@@ -11,11 +11,21 @@
                                            (st/split #" ")
                                            (next)
                                            ((partial st/join " " )))]
-    (format "%s->%s: %s %s"
-            (if-not (nil? who) (name who) "REPL")
-            (name component-key)
-            fn-name
-            formatted-args)))
+    {:who (if-not (nil? who) (name who) "REPL")
+     :id (name component-key)
+     :fn-name fn-name
+     :fn-args formatted-args}))
+
+(defn function-invocation
+  [*fn* this args]
+  (let [{:keys [id who fn-name fn-args]} (extract-data *fn* this args)]
+    (format "%s->%s: %s %s" who id fn-name fn-args)))
+
+(defn function-return
+  [*fn* this args]
+  (let [{:keys [id who fn-name fn-args]} (extract-data *fn* this args)]
+    (format "%s->%s:" id who)))
+
 
 (defn logging-function-invocation
   [*fn* this & args]
