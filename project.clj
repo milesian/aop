ae97c668ad8764f64b0fa676567888aa7cb28c3c
tangrammer <juanantonioruz@gmail.com>
2014-12-10 17:10:32 +0100

    Prepare for 0.1.3 development

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject milesian/aop "0.1.2-SNAPSHOT"
+(defproject milesian/aop "0.1.3-SNAPSHOT"
   :description "milesian bigbang AOP actions"
   :url "https://github.com/milesian/aop"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [tangrammer/defrecord-wrapper "0.1.4-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[milesian/system-examples "0.1.1-SNAPSHOT"]]}}
+                 [tangrammer/defrecord-wrapper "0.1.5"]]
+  :profiles {:dev {:dependencies [[milesian/system-examples "0.1.1"]]}}
   )
