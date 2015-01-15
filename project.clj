fa9734d34f1868e9edd4a5ea3af07d3cc1bc90fb
tangrammer <juanantonioruz@gmail.com>
2015-01-15 12:37:30 +0100

    Update dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [tangrammer/defrecord-wrapper "0.1.5"]]
+                 [tangrammer/defrecord-wrapper "defrecord-wrapper-0.1.5-20150115.113546-1"]]
   :profiles {:dev {:dependencies [[milesian/system-examples "0.1.1"]]}}
   )
